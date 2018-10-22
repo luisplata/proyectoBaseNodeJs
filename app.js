ce424ae533b90a75e91fa818ffcce0f95725c742
Luis Plata <www.luisplata@gmail.com>
2018-10-22 03:50:40 -0500

    extendiendo el poryecto base para agregarle los logs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var entorno = require('./utilitys/config-modules.js').config();
 var mysql = require("./utilitys/conexion");
+var logger = require("./utilitys/logs").log();
 
 //configuracion
 app.use(bodyParser.json());
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //importacion de rutas
 app.get("/",function(req,res){
+	logger.info("log :)");
 	res.json("Hola bby");
 });
 
